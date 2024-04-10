ca23a0ba74dd691e58113fb8249cc606eb6540c0
Timo de Jong <tjdejong98@gmail.com>
2024-04-10 15:33:04 +0200

    Adding admin panel + image models

--- galleries/views.py
+++ galleries/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from os import listdir
+from pathlib import Path
+from django.conf import settings
 # Create your views here.
 
 
 def galleries(request):
+    # path = Path(r"")
+    # images = [img for img in path.iterdir()]
+    # images = listdir(r"theme\static\media\gallerie")
     return render(request, "galleries.html")
--- projectf/settings.py
+++ projectf/settings.py
@@ -146,10 +146,9 @@ STATIC_URL = 'static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-#     BASE_DIR / "static_src/src"
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
